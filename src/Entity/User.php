98ae01315a1e63b8b538d133b22e2d2b077559c9
edetmaxence <edet.maxence@gmail.com>
2022-06-16 16:53:19 +0200

    Gestion articles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,18 +6,25 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 15)]
-    private $pseudonyme;
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $email;
+
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    private $password;
 
     #[ORM\Column(type: 'string', length: 50)]
     private $lastname;
@@ -28,14 +35,11 @@ class User
     #[ORM\Column(type: 'string', length: 5)]
     private $postcode;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 100)]
     private $city;
 
-    #[ORM\Column(type: 'string', length: 15)]
-    private $phone;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $email;
+    #[ORM\Column(type: 'string', length: 10)]
+    private $phonenumber;
 
     #[ORM\OneToMany(mappedBy: 'owner', targetEntity: Article::class)]
     private $articles;
@@ -50,16 +54,69 @@ class User
         return $this->id;
     }
 
-    public function getPseudonyme(): ?string
+    public function getEmail(): ?string
     {
-        return $this->pseudonyme;
+        return $this->email;
     }
 
-    public function setPseudonyme(string $pseudonyme): self
+    public function setEmail(string $email): self
     {
-        $this->pseudonyme = $pseudonyme;
+        $this->email = $email;
 
         return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getLastname(): ?string
@@ -110,26 +167,14 @@ class User
         return $this;
     }
 
-    public function getPhone(): ?string
+    public function getPhonenumber(): ?string
     {
-        return $this->phone;
+        return $this->phonenumber;
     }
 
-    public function setPhone(string $phone): self
+    public function setPhonenumber(string $phonenumber): self
     {
-        $this->phone = $phone;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+        $this->phonenumber = $phonenumber;
 
         return $this;
     }
