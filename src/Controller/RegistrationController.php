3a776509a20684a0e7ea1aafd118ac4c18c3fb2f
edetmaxence <edet.maxence@gmail.com>
2022-06-17 12:03:16 +0200

    register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('_profiler_home');
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('registration/register.html.twig', [
